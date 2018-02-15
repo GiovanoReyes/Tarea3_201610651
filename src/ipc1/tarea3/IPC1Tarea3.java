e763a5160fcbeb6f2debade8b1a90acf58a7361e
Giovanni <Giovanni@Giovano>
2018-02-15 15:55:49 -0600

    Ciclo menú principal

--- src/ipc1/tarea3/IPC1Tarea3.java
+++ src/ipc1/tarea3/IPC1Tarea3.java
@@ -13,9 +13,11 @@ public class IPC1Tarea3 {
         
         
         int ingreso = 0;
-                        
+        boolean x = false; // usada para poder regresar al menu principal
+    
+    
         Scanner sc = new Scanner(System.in);
-       
+    do {
         System.out.println("MENÚ:\n  1. Usuarios\n  "
                 + "2. Contador de Dígitos\n  3. Tres números de mayor a menor\n "
                 + " 4. Calcular Promedio\n  5. Salir\n ");
@@ -23,7 +25,8 @@ public class IPC1Tarea3 {
         System.out.print("Ingresar opción: "); 
         ingreso = sc.nextInt();  
 
-        switch (ingreso) {
+      
+          switch (ingreso) {
             
             case 1:
                System.out.println("Usuarios\n ");
@@ -35,29 +38,38 @@ public class IPC1Tarea3 {
                           + "3. Mostrar Usuarios Descendente\n "
                           + "4. Menú Principal\n");
                   
-                  System.out.println("No:"); 
+                  System.out.print("No:"); 
                   n = sc.nextInt();
-                
+            
+            
                     switch (n) {
                 
                         case 1:
-                            System.out.println("Ingresar Usuarios");  
                             
+                            System.out.println("\n\n\n");
+                            System.out.println("Ingresar Usuarios");
+                            x = false;
                         break;    
-                
+                            
                         
                         case 2:
                             
+                            System.out.println("2. Mostrar Usuarios Ascendente");
+                            x = false;                            
                         break;
                         
                         
                         case 3:
                             
+                            System.out.println("3. Mostrar Usuarios Descendente");
+                            x = false; // para que no cumpla la condición del do while
                         break;
                 
                 
                         case 4:
                             
+                            System.out.println("4. Menú Principal");
+                            x = true; //si el usuario escoge la opción 4, x tomará el valor true 
                         break;
                 
                     }   
@@ -67,22 +79,31 @@ public class IPC1Tarea3 {
 
             case 2:
                 System.out.println("Contador de Dígitos");
+                x = false;
                 break;
+                
 
             case 3:
                 System.out.println("Tres números de mayor a menor");
+                x = false;
                 break;
                 
             case 4:
                 System.out.println("Calcular Promedio");
+                x = false;
                 break;
 
             default:
             
                 System.out.println("Adiós");
+                x = false;
                 break;
         }
+          
+     }   while (x == true ); // Comparando el valor de la variable x
         
-    }
+    } 
+      
+      
     
 }
