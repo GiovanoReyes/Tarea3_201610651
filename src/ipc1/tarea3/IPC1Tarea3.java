2b7c9be1765633dfc1de7f8ac0eb33585dfa2ad4
Giovanni <Giovanni@Giovano>
2018-02-21 21:56:13 -0600

    Falta Submenú 1 y 4

--- src/ipc1/tarea3/IPC1Tarea3.java
+++ src/ipc1/tarea3/IPC1Tarea3.java
@@ -17,9 +17,9 @@ public class IPC1Tarea3 {
         boolean x = false; // usada para poder regresar al menu principal
     
     
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // escaner para el ingreso de datos
     do {
-        System.out.println("MENÚ:\n  1. Usuarios\n  "
+        System.out.println("__________MENÚ PRINCIPAL__________\n  1. Usuarios\n  "
                 + "2. Contador de Dígitos\n  3. Tres números de mayor a menor\n "
                 + " 4. Calcular Promedio\n  5. Salir\n ");
                 
@@ -30,7 +30,7 @@ public class IPC1Tarea3 {
           switch (ingreso) {
             
             case 1:
-               System.out.println("Usuarios\n ");
+               System.out.println("\n\n__________1. Usuarios__________\n ");
 
                  int n = 0;
                  
@@ -72,7 +72,6 @@ public class IPC1Tarea3 {
                         case 4:
                             
                             System.out.println("\n\n\n");
-                            System.out.println("4. Menú Principal");
                             x = true; //si el usuario escoge la opción 4, x tomará el valor true 
                         break;
                 
@@ -84,23 +83,18 @@ public class IPC1Tarea3 {
             case 2:
                 
                 System.out.println("\n\n\n");
-                System.out.println("Contador de Dígitos\n");
-                x = false;
-                
+                               
                 
                 int nu = 0;
                 int a = 0;
                 double b = 0 ;
-                boolean val = false;
+                boolean val = false; //Para regresar al Submenu Contador de Digitos
                  
                 do {  
-                    
-                  
-                 
-                System.out.println("Contador de Dígitos\n");
-                System.out.println(" 1. Ingresar número\n "
-                          + "2. Mostrar número de dígitos\n "
-                          + "3. Menú Principal\n");
+                    System.out.println("__________2. Contador de Dígitos__________\n");
+                    System.out.println(" 1. Ingresar número\n "
+                    + "2. Mostrar número de dígitos\n "
+                     + "3. Menú Principal\n");
                   
                   System.out.print("Ingresar opción:"); 
                   nu = sc.nextInt();
@@ -111,7 +105,7 @@ public class IPC1Tarea3 {
                       
                       case 1:
                                           
-                        System.out.println("\n\n\nIngresar un número"); 
+                        System.out.println("\nIngresar un número"); 
                         a = sc.nextInt();
                         
                         if((a>0)&&(a<1000000)){
@@ -122,11 +116,10 @@ public class IPC1Tarea3 {
                         x = false;
                         
                         }
-                        
                         else{
-                            System.out.println("Rango excedido, ingrese un número dentro del rango (0 - 100000)");
-                            val = true;
+                            System.err.println("\n\nRango excedido, ingrese un número dentro del rango (0 - 100000)");
                             x = false;
+                            val = true;
                         }
                       break;
                                            
@@ -134,35 +127,129 @@ public class IPC1Tarea3 {
                           
                           System.out.println("\n\n\n El número "+ a +" tiene: " + b +" dígitos\n");  
                           x = false;
-                          val = false;
+                          val = true;
                           break;
                          
                           
                       case 3:
                          System.out.println("\n\n\n");
-                         System.out.println("4. Menú Principal");
                          x = true;
+                         val = false;
+                         break;
                  }
                   
                                   
-            } while(val == true);
+            } while(val == true); // Para ciclo de Submenu Contador de digitos
                   
-                break;
-                
+            break;        
 
             case 3:
-                System.out.println("Tres números de mayor a menor");
-                x = false;
+                
+                System.out.println("\n\n\n");
+                
+                int num = 0; // variable para el switch
+                int n1 = 0;
+                int n2 = 0;
+                int n3 = 0;
+                boolean val_1=false; //Para regresar al Submenu Tres numeros de mayor a menor
+                
+                                
+                do {
+                
+                    System.out.println("__________Tres números de mayor a menor__________");
+                    System.out.println(" 1. Ingresar números\n "
+                    + "2. Mostrar ordenados\n "
+                     + "3. Menú Principal\n");
+                  
+                  System.out.print("Ingresar opción:"); 
+                  num = sc.nextInt();
+                  System.out.println("\n");  
+                  
+                  
+                  switch (num){
+                  
+                      case 1:
+                      
+                        System.out.println("\n Ingresar no.1: ");
+                        n1 = sc.nextInt();
+                        
+                        System.out.println("\n Ingresar no.2: ");
+                        n2 = sc.nextInt();
+                        
+                        System.out.println("\n Ingresar no.3: ");
+                        n3 = sc.nextInt();
+                        
+                        val_1 = true; 
+                        x = false;    
+                      break;
+                      
+                      case 2:
+                          
+                          if( (n3>n2)&&(n2>n1) ){
+                            System.out.println("Mayor: "+n3);  
+                            System.out.println("Medio: "+n2);
+                            System.out.println("Menor: "+n1);         
+                           } 
+        
+                            else
+                                if((n2>n3)&&(n3>n1)){
+                                    System.out.println("Mayor: "+n2);  
+                                    System.out.println("Medio: "+n3);
+                                    System.out.println("Menor: "+n1);
+                                }       
+                           if ((n3>n1)&&(n1>n2)){
+                                System.out.println("Mayor: "+n3);  
+                                System.out.println("Medio: "+n1);
+                                System.out.println("Menor: "+n2);
+                            }
+                              else 
+                                 if((n2>n1)&&(n1>n3)){
+                                    System.out.println("Mayor: "+n2);  
+                                    System.out.println("Medio: "+n1);
+                                    System.out.println("Menor: "+n3);
+                                 }
+                        if((n1>n2)&&(n2>n3)){
+                            System.out.println("Mayor: "+n1);  
+                            System.out.println("Medio: "+n2);
+                            System.out.println("Menor: "+n3);
+                        }
+                          else
+                            if((n1>n3)&&(n3>n2)){
+                                System.out.println("Mayor: "+n1);  
+                                System.out.println("Medio: "+n3);
+                                System.out.println("Menor: "+n2);
+            
+            }
+                      val_1 = true; 
+                      x = false;         
+                      break;
+                      
+                      case 3:
+                          
+                      System.out.println("\n\n\n");
+                      val_1 = false; 
+                      x = true;        
+                      break;    
+                  
+                  
+                  
+                  }
+                 
+                
+                } while(val_1==true);
+                
+                
+                                   
                 break;
                 
             case 4:
-                System.out.println("Calcular Promedio");
+                System.out.println("__________4. Calcular Promedio__________");
                 x = false;
                 break;
 
             default:
             
-                System.out.println("Adiós");
+                System.out.println("\n\n ------Adiós-------");
                 x = false;
                 break;
         }
